ff6c3fb03619960fbe0937e0c8a2d6f946fe8654
桑东波 <dongbo.sang@blue-dot.cn>
2024-02-28 15:03:54 +0800

    fix: 提交代码修改

--- config/proxy.ts
+++ config/proxy.ts
@@ -13,7 +13,7 @@ export default {
   // 如果需要自定义本地开发服务器  请取消注释按需调整
   dev: {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
-    '/central1/': {
+    '/central/': {
       // 要代理的地址
       target: 'http://127.0.0.1:8000',
       // 配置了这个可以从 http 代理到 https
